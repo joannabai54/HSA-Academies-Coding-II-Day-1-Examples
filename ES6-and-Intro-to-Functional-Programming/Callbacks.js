5dbcddd52c664c89273b0dfc39c1d956fffdb5d5
Joanna Bai <joannabai54@gmail.com>
2021-06-28 17:06:23 -0400

    higher-order-functions

--- ES6-and-Intro-to-Functional-Programming/Callbacks.js
+++ ES6-and-Intro-to-Functional-Programming/Callbacks.js
@@ -1,3 +1,9 @@
+/* alerts aren't going to work in VSCode, so unfortunately we are unable to run this code.
+The basic idea is that callback() is a first-order function that can get passed into 
+a higher-level function (in this case, doHomework) and perform some event right after 
+another.
+*/
+
 function doHomework(subject, callback) {
     alert(`Starting my ${subject} homework.`)
     callback()
@@ -8,4 +14,5 @@ function alertFinished() {
 }
 
 doHomework("english", alertFinished)
+
 
--- ES6-and-Intro-to-Functional-Programming/Higher-Order-Functions.js
+++ ES6-and-Intro-to-Functional-Programming/Higher-Order-Functions.js
@@ -1,10 +1,17 @@
+/* Array names using filter, which requires a first-order function to be plugged in
+that has some conditional which will be applied to each element within the array.
+startsWith is a unique function to strings in Javascript
+*/
 const names = ["Bliss", "Jude", "Jada", "Diego"]
 
 const startsWithJ = names.filter(name => name.startsWith("J"))
 
 console.log(startsWithJ)
 
+/* Array numbers using filter with a different type of conditional than we have up above.
+*/
 const numbers = [42, 57, 1, 4, 92, 340, 238, 4]
+
 const lessThan100 = numbers.filter(numbers => numbers < 100)
 
 console.log(lessThan100)
