12fe93fb3bad2809e9e5b3d89c9b79c50d2b9f0a
Joanna Bai <joannabai54@gmail.com>
2021-06-28 18:22:07 -0400

    complete

--- ES6-and-Intro-to-Functional-Programming/Template-Literals.js
+++ ES6-and-Intro-to-Functional-Programming/Template-Literals.js
@@ -1,3 +1,12 @@
+/* Template literals are a very streamlined way to insert variable values within string statements
+As opposed to having to concatenate everything with +, we can use ticks (the symbol right below the esc key)
+and ${} to insert variables seamlessly
+*/
 console.log(`We can print a ${1} like this.`)
 
+const a = "apple"
+const num = 74
+const bool = true
 
+console.log(`My favorite food is an ${a}, my favorite number is ${num}, and it is ${bool} that I love to sing.`)
+// Prints My favorite food is an apple, my favorite number is 74, and it is true that I love to sing.
--- JS-Fundamentals/Objects.js
+++ JS-Fundamentals/Objects.js
@@ -1,6 +1,12 @@
+// Example of creating an example object using key-value pairs & referencing different data from it
+
 const student = {
   firstName: "Ada",
   lastName: "Lovelace",
   notes: "Literally created programming",
 }
 
+console.log(student)
+console.log(student.firstName)
+console.log(Object.keys(student)) // Returns the array of keys in the object
+console.log(Object.values(student)) // Returns the array of values in the object
