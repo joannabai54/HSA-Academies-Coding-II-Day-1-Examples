f17958890f1f4c6615b7ed198328ddcfadfb6120
Joanna Bai <joannabai54@gmail.com>
2021-06-28 17:55:47 -0400

    rest-spread

--- ES6-and-Intro-to-Functional-Programming/Rest-Spread.js
+++ ES6-and-Intro-to-Functional-Programming/Rest-Spread.js
@@ -6,7 +6,6 @@ const [a, b, ...rest] = array
 console.log(a * b)
 
 console.log(rest) // Return an array
-
 console.log(...rest) // Adding ... returns separate values
 
 // Spread
@@ -19,6 +18,8 @@ function sum(x, y, z) {
 // Arrow Notation
 const sum = (x, y, z) => x + y + z
 
+// Sum expects three separate inputs (x, y, z)
+// Using the ... makes it so the numbers array is a valid input into sum
+// because it breaks the array into separate values
 const numbers = [1, 2, 3]
-
 console.log(sum(...numbers))
