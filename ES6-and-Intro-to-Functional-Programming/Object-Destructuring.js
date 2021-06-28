4e002645f41011516419afe15c090b0509864669
Joanna Bai <joannabai54@gmail.com>
2021-06-28 17:54:25 -0400

    destructuring

--- ES6-and-Intro-to-Functional-Programming/Object-Destructuring.js
+++ ES6-and-Intro-to-Functional-Programming/Object-Destructuring.js
@@ -1,3 +1,9 @@
+/* The process of destructuring an object involves defining an object
+Then you can destructure by defining an object variable 
+  const {the object keys here, separated by commas} set equal to the object
+Then you can call on the keys directly as they are defined now.
+*/
+
 const student = {
   name: "Bliss",
   house: "Kirkland",
@@ -5,6 +11,7 @@ const student = {
 
 const { name, house } = student
 
+// Note the syntax difference here! Also note the use of template-literals! 
 console.log(`${name} is in ${house}`)
 console.log(student.name + " is in " + student.house)
 
