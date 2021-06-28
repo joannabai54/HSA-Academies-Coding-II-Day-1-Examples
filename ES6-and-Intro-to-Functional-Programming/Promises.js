fc0725aa07f2109ab7e73ecb2370beb0a64bbc61
Joanna Bai <joannabai54@gmail.com>
2021-06-28 18:16:46 -0400

    promises

--- ES6-and-Intro-to-Functional-Programming/Promises.js
+++ ES6-and-Intro-to-Functional-Programming/Promises.js
@@ -1,3 +1,9 @@
+/* setTimeout is an example of an asynchronous function
+time is also given in milliseconds, so 2000 milliseconds would be 2 seconds
+This example utilizes promises that must be intiated with the "new Promise" keyword
+Promises also use ".then" to append & chain other promises onto the call
+*/
+
 function delay(t) {
     return new Promise(function(resolve){
         return setTimeout(resolve, t)
